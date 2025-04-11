3a768d69cc411ccf231c3895ecfd8fca7988c4cf
killian-lecorf <Klecorf@normandiewebschool.fr>
2025-04-11 12:39:15 +0200

    feat: Added configuration cors

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,18 @@ import "./configs/aliases";
 const brisk = new Brisk();
 
 brisk.use(bodyParser);
-brisk.use(cors);
 
-brisk.registerRouter('/users', userRoutes)
+brisk.registerRouter('/users', userRoutes) 
+
+// optionnal CORS configuration 
+const corsOptions = {
+  origin: 'http://localhost:5173',
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+}
+
+brisk.use(cors(corsOptions));
 
 connectToDatabase().then(() => {
     const PORT = 5253;
